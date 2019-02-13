9bb4ad860d99883e26c6fb118c9e75313b90a4b7
Ubuntu <ubuntu@ip-172-26-4-122.us-west-2.compute.internal>
2019-02-13 01:39:44 +0000

    ordered things around

--- dataExtraction/1Time.sh
+++ dataExtraction/1Time.sh
@@ -6,11 +6,18 @@
 #### FILES IN A DIRECTORY CALLED 'testingData'
 ####
 
+# check if there are correct number of arguments
+if [ $# -ne 1 ]; then
+	echo -e "\nplease enter test name for file creation\n"
+	exit 1
+fi
+
 echo -e "Starting test!!\n"
 
-for i in {1..20}
+for i in {1..2}
 do
   fileName="$1_test_$i.txt"
   echo -e "Completing test\t$i..."
-  ./../leveldb/build/db_bench > testingData/$fileName
+  ./../../leveldb/build/db_bench > testingData/$fileName
+  ./a.out
 done
