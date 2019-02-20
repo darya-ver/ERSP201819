6f22cd9d9c8f462e4044873954370e8852e69c8d
Darya Verzhbinsky <darya99@gmail.com>
2019-02-19 20:47:25 -0800

    put to python list

--- dataExtraction/getLists.py
+++ dataExtraction/getLists.py
@@ -1,9 +1,62 @@
 def main():
   fp = open('pythonLists.txt')
-  for i, line in enumerate(fp.readLines() ):
+
+  minsT = []
+  onesT = []
+  twosT = []
+  threesT = []
+  maxsT = []
+
+  minsB = []
+  onesB = []
+  twosB = []
+  threesB = []
+  maxsB = []
+  
+  for i, line in enumerate(fp.readlines() ):
     # do something here
 
+    #print( i )
+    #print( line )
 
+    split = line.split( ',', -1)
+    split = split[:-1]
+    if( i != 0 ):
+      for str in range(0, len(split) ):
+        split[i] = float(split[i])
+
+    if( i == 1 ):
+      minsT = split
+    elif( i == 2 ):
+      onesT = split
+    elif( i == 3 ):
+      twosT = split
+    elif( i == 4 ):
+      threesT = split
+    elif( i == 5 ):
+      maxsT = split
+    elif( i == 6 ):
+      minsB = split
+    elif( i == 7 ):
+      onesB = split
+    elif( i == 8 ):
+      twosB = split
+    elif( i == 9 ):
+      threesB = split
+    elif( i == 10 ):
+      maxsB = split
+
+  print(minsT)
+  print(onesT)
+  print(twosT)
+  print(threesT)
+  print(maxsT)
+
+  print(minsB)
+  print(onesB)
+  print(twosB)
+  print(threesB)
+  print(maxsB)
 
 if __name__ == '__main__':
   main()
--- dataExtraction/main2.cpp
+++ dataExtraction/main2.cpp
@@ -13,14 +13,120 @@ int main() {
   dataVector vect; 
   vect.readDirectory( dirName );
 
+  
+  vector<string> names;
+  vector<float> minsT;
+  vector<float> onesT;
+  vector<float> twosT;
+  vector<float> threesT;
+  vector<float> maxsT;
+
+  vector<float> minsB;
+  vector<float> onesB;
+  vector<float> twosB;
+  vector<float> threesB;
+  vector<float> maxsB;
+
   // loop through all the nodes and calculate quartiles
   for( auto it2 = vect.allData.begin(); it2 != vect.allData.end(); it2 ++ ) {
     it2->second->calculateQuartiles();
+
+    names.push_back( it2->second->testName );
+    // throughput variables
+    minsT.push_back( it2->second->tQ[0] );
+    onesT.push_back( it2->second->tQ[1] );
+    twosT.push_back( it2->second->tQ[2] );
+    threesT.push_back( it2->second->tQ[3] );
+    maxsT.push_back( it2->second->tQ[4] );
+
+    // bandwidth variables
+    minsB.push_back( it2->second->bQ[0] );
+    onesB.push_back( it2->second->bQ[1] );
+    twosB.push_back( it2->second->bQ[2] );
+    threesB.push_back( it2->second->bQ[3] );
+    maxsB.push_back( it2->second->bQ[4] );
+
   }
 
+  //for( auto it = 
+  
   ofstream outfile("pythonLists.txt", ofstream::out );
+  
+  // writing all the lists to a file 
+  for(int i = 0; i < names.size(); i++ ) {
+    string num = names[i] + ",";
+    outfile.write( num.c_str(), num.length() );
+  }
+  outfile.write( "\n", 1 );
+  /*
+   * THROUGPUT
+   */
+  for(int i = 0; i < minsT.size(); i++ ) {
+    string num = to_string(minsT[i]) + ",";
+    outfile.write( num.c_str(), num.length() );
+  }
+  outfile.write( "\n", 1 );
 
-  vect.writeToFilePython( outfile );
+  for(int i = 0; i < onesT.size(); i++ ) {
+    string num = to_string(onesT[i]) + ",";
+    outfile.write( num.c_str(), num.length() );
+  }
+  outfile.write( "\n", 1 );
+
+  for(int i = 0; i < twosT.size(); i++ ) {
+    string num = to_string(twosT[i]) + ",";
+    outfile.write( num.c_str(), num.length() );
+  }
+  outfile.write( "\n", 1 );
+
+  for(int i = 0; i < threesT.size(); i++ ) {
+    string num = to_string(threesT[i]) + ",";
+    outfile.write( num.c_str(), num.length() );
+  }
+  outfile.write( "\n", 1 );
+
+  for(int i = 0; i < maxsT.size(); i++ ) {
+    string num = to_string(maxsT[i]) + ",";
+    outfile.write( num.c_str(), num.length() );
+  }
+  outfile.write( "\n", 1 );
+
+
+  /*
+   * BANDWIDTHS
+   */
+  for(int i = 0; i < minsB.size(); i++ ) {
+    string num = to_string(minsB[i]) + ",";
+    outfile.write( num.c_str(), num.length() );
+  }
+  outfile.write( "\n", 1 );
+
+  for(int i = 0; i < onesB.size(); i++ ) {
+    string num = to_string(onesB[i]) + ",";
+    outfile.write( num.c_str(), num.length() );
+  }
+  outfile.write( "\n", 1 );
+
+  for(int i = 0; i < twosB.size(); i++ ) {
+    string num = to_string(twosB[i]) + ",";
+    outfile.write( num.c_str(), num.length() );
+  }
+  outfile.write( "\n", 1 );
+
+  for(int i = 0; i < threesB.size(); i++ ) {
+    string num = to_string(threesB[i]) + ",";
+    outfile.write( num.c_str(), num.length() );
+  }
+  outfile.write( "\n", 1 );
+
+  for(int i = 0; i < maxsB.size(); i++ ) {
+    string num = to_string(maxsB[i]) + ",";
+    outfile.write( num.c_str(), num.length() );
+  }
+  outfile.write( "\n", 1 );
+
+
+  //vect.writeToFilePython( outfile );
 
   outfile.close();
 
