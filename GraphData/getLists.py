5b8e7b21abee9c9e18142411af4d08f52f0fda48
Darya Verzhbinsky <darya99@gmail.com>
2019-02-27 08:59:30 -0800

    changed getLists.py to get list of lists

--- GraphData/getLists.py
+++ GraphData/getLists.py
@@ -1,75 +1,105 @@
-from displayGraph import drawEbar, drawBoxPlot
-import numpy as np
+#from displayGraph import drawEbar, drawBoxPlot
+#import numpy as np
 
 def main():
-	# open file
-	fp = open('../DataExtraction/pythonLists.txt')
 
-	# create arrays to store results
-	minsT = []
-	onesT = []
-	twosT = []
-	threesT = []
-	maxsT = []
+  # create arrays to store results
+  allDataTCurr = [] # throughputs for curr kernel
+  allDataBCurr = [] # bandwidths  for curr kernel
 
-	minsB = []
-	onesB = []
-	twosB = []
-	threesB = []
-	maxsB = []
+  allDataTOld = []  # throughputs for old  kernel
+  allDataBOld = []  # bandwidths  for old  kernel
 
-	allData = []
-	tests = []
+  tests = []        # array of names
 
-	# iterate through all lines
-	for i, line in enumerate(fp.readlines() ):
+  ########################
+  ###   CURR KERNEL
+  ########################
 
-		# split by comma
-		split = line.split( ',', -1)
-		split = split[:-1]
-			
-		# cast each to a float except for test names
-		if( i != 0 ):
-			for j in range(0, len(split) ):
-				split[j] = float(split[j])
+  # open file
+  fp = open('../DataExtraction/pythonListsCurr.txt')
+  # iterate through all lines
+  for i, line in enumerate(fp.readlines() ):
 
-		# store names of each benchmark
-		if( i == 0 ):
-			tests = split
+    # split by comma
+    split = line.split( ',', -1)
+    split = split[:-1] # gets rid of extra char at the end
+      
+    # cast each to a float except for test names
+    if( i != 0 and i != 1 and i != 17 ):
+      for j in range(0, len(split) ):
+        split[j] = float(split[j])
 
-		# store data
-		if( i == 1 ):
-			minsT = split
-		elif( i == 2 ):
-			onesT = split
-		elif( i == 3 ):
-			twosT = split
-		elif( i == 4 ):
-			threesT = split
-		elif( i == 5 ):
-			maxsT = split
-		elif( i == 6 ):
-			minsB = split
-		elif( i == 7 ):
-			onesB = split
-		elif( i == 8 ):
-			twosB = split
-		elif( i == 9 ):
-			threesB = split
-		elif( i == 10 ):
-			maxsB = split
+    # store names of each benchmark
+    if i == 0 :
+      tests = split
+      continue
 
-	# convert all lists to arrays
-	minsT = np.asarray(minsT)
-	onesT = np.asarray(onesT)
-	twosT = np.asarray(twosT)
-	threesT = np.asarray(threesT)
-	maxsT = np.asarray(maxsT)
+    # skip over throughput and bandwidth string in file
+    if i == 1 or i == 17:
+      continue
 
-	tests = np.asarray(tests)
+    # extract for throughputs
+    if( i < 17 ):
+      allDataTCurr.append( split )
 
-	#drawEbar(tests, minsT, twosT, maxsT)
-	drawBoxPlot(tests, minsT, onesT, twosT, threesT, maxsT)
+    # extract for bandwidths 
+    if( i > 17 ):
+      allDataBCurr.append( split )
+
+  ########################
+  ###   OLD KERNEL
+  ########################
+
+  # open file
+  fp = open('../DataExtraction/pythonListsOld.txt')
+
+  # iterate through all lines
+  for i, line in enumerate(fp.readlines() ):
+
+    # split by comma
+    split = line.split( ',', -1)
+    split = split[:-1] # gets rid of extra char at the end
+      
+    # cast each to a float except for test names
+    if( i != 0 and i != 1 and i != 17 ):
+      for j in range(0, len(split) ):
+        split[j] = float(split[j])
+
+    # skip over throughput and bandwidth string in file
+    if i == 0 or i == 1 or i == 17:
+      continue
+
+    # extract for throughputs
+    if( i < 17 ):
+      allDataTOld.append( split )
+
+    # extract for bandwidths 
+    if( i > 17 ):
+      allDataBOld.append( split )
+
+  ########################
+  ###  GRAPH STUFF 
+  ########################
+
+  print( tests )
+
+  print( allDataTCurr )
+  print( allDataBCurr )
+
+  print( allDataTOld )
+  print( allDataBOld )
+
+  # convert all lists to arrays
+  #tests = np.asarray(tests)
+
+  #allDataTCurr = np.asarray(allDataTCurr)
+  #allDataBCurr = np.asarray(allDataBCurr)
+  #allDataTOld = np.asarray(allDataTOld)
+  #allDataBOld = np.asarray(allDataBOld)
+
+  #drawEbar(tests, minsT, twosT, maxsT)
+  #drawBoxPlot(tests, minsT, onesT, twosT, threesT, maxsT)
 
 if __name__ == '__main__':
-	main()
+  main()
